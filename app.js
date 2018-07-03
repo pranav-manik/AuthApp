d93f3f2f3b26434e63d7a587f34ccc8e3e9c826b
pranav-manik <pranav.manik@gmail.com>
2018-07-03 13:50:05 -0400

    body-parser

--- app.js
+++ app.js
@@ -2,8 +2,11 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-var MongoClient = require('mongodb').MongoClient;
+//var MongoClient = require('mongodb').MongoClient;
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 var url = "mongodb://localhost:27017/";
 
 /*MongoClient.connect(url, function(err, db) {
@@ -21,6 +24,16 @@ var url = "mongodb://localhost:27017/";
 mongoose.connect('mongodb://localhost/auth');
 var db = mongoose.connection;
 
+//use sessions for tracking logins
+app.use(session({
+  secret: 'work hard',
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
 //handle mongo error
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
@@ -28,6 +41,9 @@ db.once('open', function () {
 });
 
 
+// parse incoming requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //for static site
 app.use('/',express.static('views'))
@@ -43,7 +59,7 @@ app.use(function (req, res, next) {
   next(err);
 });
 
-
+//listen on port
 app.listen(3000, function () {
 	console.log('running on port 3000 boiii!');
     });
